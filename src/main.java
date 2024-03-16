53d301e977c19bf5be13cb4a382eb6f727d4cc7a
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2024-03-16 11:03:20 +0530

    start creating navbar

--- src/main.java
+++ src/main.java
@@ -1,39 +1,17 @@
-import java.awt.Color;
-import java.awt.Font;
-
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.border.Border;
+
 
 public class main {
 
-    public static void main(String[] args) {
-
-        Border border = BorderFactory.createLineBorder(Color.red);
-
-        ImageIcon image = new ImageIcon("s2.png");
+    public static void main(String[] args){
         JLabel label = new JLabel();
-        label.setText("Bro, FUCK THIS!!");
-        label.setIcon(image);
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        label.setForeground(new Color(0xc61a09));
-        label.setFont(new Font("MV Boli",Font.PLAIN,20));
-        label.setIconTextGap(-20);
-        label.setBackground(Color.black);
-        label.setOpaque(true);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setText("Hey, Tell us What you're looking for...");
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setVisible(true);
         frame.add(label);
-
     }
-
 }
